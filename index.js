6350d5212b255e763ea932f61022955975c4a0e3
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-06-13 21:45:51 -0600

    Step 4 complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function addMovie(event) {
     const movie = document.createElement('li');
     const movieTitle = document.createElement('span');
     movieTitle.textContent = inputField.value
+    movieTitle.addEventListener('click', crossOffMovie)
 
     movie.appendChild(movieTitle)
     const deleteBtn = document.createElement('button')
@@ -16,6 +17,10 @@ function addMovie(event) {
 
 function deleteMovie(event) {
     event.target.parentNode.remove()
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
 }
 
 document.querySelector('form').addEventListener('submit',addMovie)
