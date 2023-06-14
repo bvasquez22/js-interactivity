938baf393fbdfca37a04bc9b08516d7b5f41125e
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-06-13 21:38:17 -0600

    Step 2 Complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+function addMovie(event) {
+    event.preventDefault()
+    const inputField = document.querySelector('input');
+    const movie = document.createElement('li');
+    const movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value
+
+    movie.appendChild(movieTitle)
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ''
+}
+
+document.querySelector('form').addEventListener('submit',addMovie)
